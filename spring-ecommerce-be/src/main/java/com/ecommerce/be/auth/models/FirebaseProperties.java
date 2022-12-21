503aea9062aa8a2564b6673a0cd98824436d0271
Vaibhav Jadhav <vaibhav.jadhav@idrive.com>
2022-12-21 12:40:43 +0530

    firebase authentication implemented

---
+++ spring-ecommerce-be/src/main/java/com/ecommerce/be/auth/models/FirebaseProperties.java
@@ -0,0 +1,14 @@
+package com.ecommerce.be.auth.models;
+
+import lombok.Data;
+
+@Data
+public class FirebaseProperties {
+
+	int sessionExpiryInDays;
+	String databaseUrl;
+	boolean enableStrictServerSession;
+	boolean enableCheckSessionRevoked;
+	boolean enableLogoutEverywhere;
+
+}
---
+++ spring-ecommerce-be/src/main/java/com/ecommerce/be/config/FirebaseConfig.java
@@ -0,0 +1,48 @@
+package com.ecommerce.be.config;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ClassPathResource;
+
+import com.ecommerce.be.auth.models.SecurityProperties;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Autowired
+    SecurityProperties secProps;
+
+    @Primary
+    @Bean
+    public void firebaseInit() {
+        InputStream inputStream = null;
+        try {
+			inputStream = new ClassPathResource("firebase-config.json").getInputStream();
+        } catch (IOException e3) {
+            e3.printStackTrace();
+        }
+        try {
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(inputStream))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+            System.out.println("Firebase Initialize");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
